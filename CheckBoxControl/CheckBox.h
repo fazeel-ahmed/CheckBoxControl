df92d5ead6ec84c0f9a3b14ec56648f6fdb12aab
Fazeel <fazeel.ahmed@eres.ae>
2017-02-11 22:15:48 +0400

    initial commit

---
+++ CheckBoxControl/CheckBox.h
@@ -0,0 +1,29 @@
+//
+//  CheckBox.h
+//  TestApp
+//
+//  Created by Fazeel on 1/21/17.
+//  Copyright © 2017 Fazeel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CheckBox : UIControl
+
+
+-(void)setChecked:(BOOL)isChecked;
+-(void)setEnabled:(BOOL)isEnabled;
+-(void)setText:(NSString *)stringValue;
+
+@property IBInspectable UIColor *checkColor;
+@property IBInspectable UIColor *boxFillColor;
+@property IBInspectable UIColor *boxBorderColor;
+@property IBInspectable UIFont *labelFont;
+@property IBInspectable UIColor *labelTextColor;
+
+@property IBInspectable BOOL isEnabled;
+@property IBInspectable BOOL isChecked;
+@property IBInspectable BOOL showTextLabel;
+@property (nonatomic, strong) IBInspectable  NSString *text;
+
+@end
---
+++ CheckBoxControl/CheckBoxControl.h
@@ -0,0 +1,19 @@
+//
+//  CheckBoxControl.h
+//  CheckBoxControl
+//
+//  Created by Fazeel on 1/22/17.
+//  Copyright © 2017 Fazeel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CheckBoxControl.
+FOUNDATION_EXPORT double CheckBoxControlVersionNumber;
+
+//! Project version string for CheckBoxControl.
+FOUNDATION_EXPORT const unsigned char CheckBoxControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CheckBoxControl/PublicHeader.h>
+
+#import <CheckBoxControl/CheckBox.h>
